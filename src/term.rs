3b83c5c1544c3c31b533c1a872e4437b90118cec
Sharad Chand <sharad.d.chand@gmail.com>
2018-04-24 11:35:55 +0545

    Convenient term! macro

--- src/term.rs
+++ src/term.rs
@@ -101,6 +101,88 @@ generate_tokens! {
     Mixin => "mixin"
 }
 
+#[macro_export]
+macro_rules! term {
+    (OpenParen) => { $crate::term::OpenParen };
+    (CloseParen) => { $crate::term::CloseParen };
+    (OpenBracket) => { $crate::term::OpenBracket };
+    (CloseBracket) => { $crate::term::CloseBracket };
+    (OpenBrace) => { $crate::term::OpenBrace };
+    (CloseBrace) => { $crate::term::CloseBrace };
+    (,) => { $crate::term::Comma };
+    (-) => { $crate::term::Minus };
+    (.) => { $crate::term::Dot };
+    (...) => { $crate::term::Ellipsis };
+    (:) => { $crate::term::Colon };
+    (;) => { $crate::term::SemiColon };
+    (<) => { $crate::term::LessThan };
+    (=) => { $crate::term::Assign };
+    (>) => { $crate::term::GreaterThan };
+    (?) => { $crate::term::QMark };
+    (or) => { $crate::term::Or };
+    (optional) => { $crate::term::Optional };
+    (attribute) => { $crate::term::Attribute };
+    (callback) => { $crate::term::Callback };
+    (const) => { $crate::term::Const };
+    (deleter) => { $crate::term::Deleter };
+    (dictionary) => { $crate::term::Dictionary };
+    (enum) => { $crate::term::Enum };
+    (getter) => { $crate::term::Getter };
+    (includes) => { $crate::term::Includes };
+    (inherit) => { $crate::term::Inherit };
+    (interface) => { $crate::term::Interface };
+    (iterable) => { $crate::term::Iterable };
+    (maplike) => { $crate::term::Maplike };
+    (namespace) => { $crate::term::Namespace };
+    (partial) => { $crate::term::Partial };
+    (required) => { $crate::term::Required };
+    (setlike) => { $crate::term::Setlike };
+    (setter) => { $crate::term::Setter };
+    (static) => { $crate::term::Static };
+    (stringifier) => { $crate::term::Stringifier };
+    (typedef) => { $crate::term::Typedef };
+    (unrestricted) => { $crate::term::Unrestricted };
+    (symbol) => { $crate::term::Symbol };
+    (-Infinity) => { $crate::term::NegInfinity };
+    (ByteString) => { $crate::term::ByteString };
+    (DOMString) => { $crate::term::DOMString };
+    (FrozenString) => { $crate::term::FrozenArray };
+    (Infinity) => { $crate::term::Infinity };
+    (NaN) => { $crate::term::NaN };
+    (USVString) => { $crate::term::USVString };
+    (any) => { $crate::term::Any };
+    (boolean) => { $crate::term::Boolean };
+    (byte) => { $crate::term::Byte };
+    (double) => { $crate::term::Double };
+    (false) => { $crate::term::False };
+    (float) => { $crate::term::Float };
+    (long) => { $crate::term::Long };
+    (null) => { $crate::term::Null };
+    (object) => { $crate::term::Object };
+    (octect) => { $crate::term::Octet };
+    (sequence) => { $crate::term::Sequence };
+    (short) => { $crate::term::Short };
+    (true) => { $crate::term::True };
+    (unsigned) => { $crate::term::Unsigned };
+    (void) => { $crate::term::Void };
+    (record) => { $crate::term::Record };
+    (ArrayBuffer) => { $crate::term::ArrayBuffer };
+    (DataView) => { $crate::term::DataView };
+    (Int8Array) => { $crate::term::Int8Array };
+    (Int16Array) => { $crate::term::Int16Array };
+    (Int32Array) => { $crate::term::Int32Array };
+    (Uint8Array) => { $crate::term::Uint8Array };
+    (Uint16Array) => { $crate::term::Uint16Array };
+    (Uint32Array) => { $crate::term::Uint32Array };
+    (Uint8ClampedArray) => { $crate::term::Uint8ClampedArray };
+    (Float32Array) => { $crate::term::Float32Array };
+    (Float64Array) => { $crate::term::Float64Array };
+    (Promise) => { $crate::term::Promise };
+    (Error) => { $crate::term::Error };
+    (readonly) => { $crate::term::ReadOnly };
+    (mixin) => { $crate::term::Mixin };
+}
+
 #[cfg(test)]
 mod test {
     macro_rules! generate_tests {
