a9c7f28f819c4f49977bb5a2a7de3811f9080bb7
Sharad Chand <sharad.d.chand@gmail.com>
2018-05-06 10:20:32 +0545

    Updated tests

--- src/literal.rs
+++ src/literal.rs
@@ -184,92 +184,92 @@ mod test {
     test!(should_parse_other_lit { "&" =>
         "";
         OtherLit;
-        value == "&";
+        0 == "&";
     });
 
     test!(should_parse_integer { "45" =>
         "";
-        i64 => 45
+        IntegerLit => IntegerLit::Dec(DecI64("45".to_string()))
     });
 
     test!(should_parse_integer_surrounding_with_spaces { "  123123  " =>
         "";
-        i64 => 123123
+        IntegerLit => IntegerLit::Dec(DecI64("123123".to_string()))
     });
 
     test!(should_parse_integer_preceeding_others { "3453 string" =>
         "string";
-        i64 => 3453
+        IntegerLit => IntegerLit::Dec(DecI64("3453".to_string()))
     });
 
     test!(should_parse_neg_integer { "-435" =>
         "";
-        i64 => -435
+        IntegerLit => IntegerLit::Dec(DecI64("-435".to_string()))
     });
 
     test!(should_parse_hex_number { "0X08" =>
         "";
-        i64 => 8
+        IntegerLit => IntegerLit::Hex(HexI64("0X08".to_string()))
     });
 
     test!(should_parse_hex_large_number { "0xA" =>
         "";
-        i64 => 10
+        IntegerLit => IntegerLit::Hex(HexI64("0xA".to_string()))
     });
 
     test!(should_parse_float { "45.434" =>
         "";
-        f64 => 45.434
+        FloatLit => FloatLit::Value("45.434".to_string())
     });
 
     test!(should_parse_float_surrounding_with_spaces { "  2345.2345  " =>
         "";
-        f64 => 2345.2345
+        FloatLit => FloatLit::Value("2345.2345".to_string())
     });
 
     test!(should_parse_float_preceeding_others { "3453.32334 string" =>
         "string";
-        f64 => 3453.32334
+        FloatLit => FloatLit::Value("3453.32334".to_string())
     });
 
     test!(should_parse_neg_float { "-435.3435" =>
         "";
-        f64 => -435.3435
+        FloatLit => FloatLit::Value("-435.3435".to_string())
     });
 
     test!(should_parse_float_exp { "5.3434e23" =>
         "";
-        f64 => 5.3434e23
+        FloatLit => FloatLit::Value("5.3434e23".to_string())
     });
 
     test!(should_parse_float_exp_with_decimal { "3e23" =>
         "";
-        f64 => 3e23
+        FloatLit => FloatLit::Value("3e23".to_string())
     });
 
     test!(should_parse_neg_infinity { "-Infinity" =>
         "";
-        f64 => ::std::f64::NEG_INFINITY
+        FloatLit => FloatLit::NegInfinity(term!(-Infinity))
     });
 
     test!(should_parse_infinity { "Infinity" =>
         "";
-        f64 => ::std::f64::INFINITY
+        FloatLit => FloatLit::Infinity(term!(Infinity))
     });
 
     test!(should_parse_string { r#""this is a string""# =>
         "";
-        String => "this is a string"
+        StringLit => StringLit("this is a string".to_string())
     });
 
     test!(should_parse_string_surround_with_spaces { r#"  "this is a string"  "# =>
         "";
-        String => "this is a string"
+        StringLit => StringLit("this is a string".to_string())
     });
 
     test!(should_parse_string_followed_by_string { r#" "this is first"  "this is second" "# =>
         r#""this is second" "#;
-        String => "this is first"
+        StringLit => StringLit("this is first".to_string())
     });
 
     test!(should_parse_null { "null" =>
@@ -284,6 +284,6 @@ mod test {
 
     test!(should_parse_bool { "true" =>
         "";
-        bool => true
+        BooleanLit => BooleanLit(true)
     });
 }
