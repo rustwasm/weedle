f1548fc043d929b0d9d2cf4a4026b38f8de0db2b
R. Andrew Ohana <andrew.ohana@gmail.com>
2018-07-21 13:06:44 -0700

    remove unneeded macro

--- src/macros.rs
+++ src/macros.rs
@@ -13,29 +13,6 @@ macro_rules! weedle {
     };
 }
 
-// Workaround to use `CompleteStr`
-macro_rules! re_capture_static (
-  ($i:expr, $re:expr) => (
-    {
-      use $crate::nom::{Err,ErrorKind,IResult};
-      use $crate::nom::Slice;
-
-      regex!(RE, $re);
-      if let Some(c) = RE.captures(&$i) {
-        let v:Vec<_> = c.iter().filter(|el| el.is_some()).map(|el| el.unwrap()).map(|m| $i.slice(m.start()..m.end())).collect();
-        let offset = {
-          let end = v.last().unwrap();
-          end.as_ptr() as usize + end.len() - $i.as_ptr() as usize
-        };
-        Ok(($i.slice(offset..), v))
-      } else {
-        let res: IResult<_,_> = Err(Err::Error(error_position!($i, ErrorKind::RegexpCapture::<u32>)));
-        res
-      }
-    }
-  )
-);
-
 // Return valid option as it is & convert `Error` to `None`
 #[macro_export]
 macro_rules! opt_flat(
